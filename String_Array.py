8e1a982bbdf0c07806a855948ebfefcc91ae165b
Brandon Huff <bhuff21@gmail.com>
2019-05-03 21:52:45 -0400

    Completed algorithm and handled exception: invalid data type

--- String_Array.py
+++ String_Array.py
@@ -1,4 +1,29 @@
 def create_array(string):
-    array = string.split(" ")
+    """
+    This function takes a string as input and returns an array of the words in the string.
+    Example:
+        create_array("This is a sentence") --> ["This", "is", "a", "sentence"]
 
-    return array
+    Parameters:
+        string == a string data type
+
+    Algorithm:
+        -Check the data type of string
+        -If string's data type is a "string" then:
+            Split string into separate words --> array[]
+            Return array
+        Else:
+            generate error message
+            return error message
+    """
+
+    # Check the data type as a string. If yes, returns bool true --> is_string. If no, returns bool false --> is_string
+    is_string = type(string) is string
+
+    # Begin logic
+    if is_string:
+        array = string.split(" ")
+        return array
+    else:
+        error_message = "Argument must be of data 'type string'"
+        return error_message
