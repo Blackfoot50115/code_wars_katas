b9941a7329bc97a9b465f6def08ae6ddd0072475
Brandon Huff <bhuff21@gmail.com>
2019-05-03 23:17:40 -0400

    Fixed style issues

--- Choose_Best_Sum.py
+++ Choose_Best_Sum.py
@@ -4,9 +4,7 @@ def choose_best_sum(t, k, ls):
         will take as parameters t (maximum sum of distances, integer >= 0), k (number of towns
         to visit, k >= 1) and ls (list of distances, all distances are positive or null integers
         and this list has at least one element). The function returns the "best" sum ie the biggest
-        possible sum of k distances less than or equal to the given limit t, if that sum exists,
-
-        List of Distances xs = [100, 76, 56, 44, 89, 73, 68, 56, 64, 123, 2333, 144, 50, 132, 123, 34, 89]
+        possible sum of k distances less than or equal to the given limit t, if that sum exists.
 
         Parameter rules:
             1.)t must be an int >= 0
@@ -39,6 +37,7 @@ def choose_best_sum(t, k, ls):
     return largest_sum
 
 
+# Just used for testing the function
 xs = [100, 76, 56, 44, 89, 73, 68, 56, 64, 123, 2333, 144, 50, 132, 123, 34, 89]
 
 best_sum = choose_best_sum(400, 3, xs)
